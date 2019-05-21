4f6536d7d5cd28995cda1a97e6c2e850a26cb65d
stevenj <jshl8612@gmail.com>
2019-05-21 22:27:54 +0900

    Add GameTests

--- Roshambo/RoshamboTests/GameTest.swift
+++ Roshambo/RoshamboTests/GameTest.swift
@@ -1,5 +1,5 @@
 //
-//  RoshamboTests.swift
+//  GameTest.swift
 //  RoshamboTests
 //
 //  Created by Shenglong Jiang on 21/05/2019.
@@ -9,27 +9,28 @@
 import XCTest
 @testable import Roshambo
 
-class RoshamboTests: XCTestCase {
+class GameTests: XCTestCase {
+    
+    var game: Game = Game()
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
     
 }
