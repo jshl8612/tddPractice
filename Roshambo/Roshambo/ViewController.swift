8dfcbf71e0a320f71ec7fd5fea18d8335f658935
stevenj <jshl8612@gmail.com>
2019-05-21 22:14:21 +0900

    Init project

---
+++ Roshambo/Roshambo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Shenglong Jiang on 21/05/2019.
+//  Copyright © 2019 Shenglong Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
